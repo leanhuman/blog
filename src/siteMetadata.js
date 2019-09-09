67ebd8a804df9f7ae04501bd17fb834c895e6156
leanhuman <theleanhuman@gmail.com>
2019-09-09 06:51:51 -0700

    update

--- src/siteMetadata.js
+++ src/siteMetadata.js
@@ -4,7 +4,7 @@ export default {
   title: "The Lean Human",
 
   author: "Jimmy Lindsey",
-  description: "An inside look into how to unlock your body and mind to function more efficiently",
+  description: "Unlock your body and mind for optimal performance and longevity.",
 
   // The number of posts to a page on the site index.
   indexPageSize: 10,
