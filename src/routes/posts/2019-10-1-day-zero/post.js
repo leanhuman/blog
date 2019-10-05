5af01fda030f388fb609f82f7f59d696c67f8571
leanhuman <theleanhuman@gmail.com>
2019-10-05 02:58:28 -0700

    update first blog post

---
+++ src/routes/posts/2019-10-1-day-zero/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Day Zero`,
+  tags: ['biomarkers', 'biohacking'],
+  spoiler: "In this post I'm going to share what I've been doing up to this point, as well as my biomarkers. I'll also share my approach for Q4 2019.",
+  getContent: () => import('./document.mdx'),
+}
