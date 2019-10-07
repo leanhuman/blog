64564365628ee3913478f6cb349d853edd340e1f
leanhuman <theleanhuman@gmail.com>
2019-10-06 19:49:54 -0700

    update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -15,7 +15,7 @@ function Bio(props) {
     `}>
       <img src={photoURL} alt="Me" />
       <p>
-        <a href="https://instagram.com/leanhuman/">Jimmy Lindsey</a> is an experiment of one. This blog provides a systems-engineering approach to unlocking your body and mind for optimal performance and longevity.
+        <a href="https://instagram.com/leanhuman/">Jimmy Lindsey</a> is a husband, father, and biohacker. As an n of 1, he has been experimenting with fitness and nutrition since 2002. Jimmy studied Applied Physiology at Columbia University and has worked in strength and conditioning, physical therapy, and biohacking. His goal is to help every human realize their unlimited potential. This blog provides a systems-engineering approach to unlocking your body and mind for optimal performance and longevity.
       </p>
     </div>
   )
