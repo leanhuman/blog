f752a4fd24994ae10e4024ffaacab81ee240825f
leanhuman <theleanhuman@gmail.com>
2019-10-06 21:03:40 -0700

    update post

--- src/routes/posts/2019-10-1-milestone-zero/post.js
+++ src/routes/posts/2019-10-1-milestone-zero/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `Day Zero`,
+  title: `Milestone Zero`,
   tags: ['biomarkers', 'biohacking'],
   spoiler: "In this post I'm going to share my biomarkers as well as my approach to biohacking.",
   getContent: () => import('./document.mdx'),
