e3f86f0633de820562ef41220e820b36c4ea3715
leanhuman <theleanhuman@gmail.com>
2019-10-08 06:22:20 -0700

    update blog posts

---
+++ src/routes/posts/2019-10-4-milestone-three/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Milestone Three`,
+  tags: ['biomarkers', 'biohacking'],
+  spoiler: "In this post I'm going to share my biomarkers as well as my approach to biohacking.",
+  getContent: () => import('./document.mdx'),
+}
