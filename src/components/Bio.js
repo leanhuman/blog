588e43aaa397c67d35cc79b93d6ce78779f1344b
leanhuman <theleanhuman@gmail.com>
2019-10-08 19:58:38 -0700

    update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -15,7 +15,7 @@ function Bio(props) {
     `}>
       <img src={photoURL} alt="Me" />
       <p>
-        <a href="https://instagram.com/leanhuman/">Jimmy Lindsey</a> started a low carbohydrate, high fat diet in 2012 while studying Applied Physiology at Columbia University. Having read a sliver of text on this "diet" in one of his textbooks, Jimmy began to question all of his teachings up to that point, which emphasized carbohydrate as the primary source of fuel for the brain and body. He ran with the assumption that the brain and body can function more optimally when fueled by fat instead of carbohydrate, and hasn't looked back.
+        <a href="https://instagram.com/leanhuman/">Jimmy Lindsey</a> started a low carbohydrate, high fat diet in 2012 while studying Applied Physiology at Columbia University. Having read a sliver of text on this "diet" in one of his textbooks, Jimmy began to question everything that he had been taught about nutrition, which emphasized carbohydrate as the primary source of fuel for the brain and body. He ran with the assumption that the brain and body can function more optimally when fueled by fat instead of carbohydrate, and hasn't looked back. He's since begun to question everything that he's been taught about fitness and has begun biohacking fitness as a way to save time and energy and promote longevity.
       </p>
     </div>
   )
